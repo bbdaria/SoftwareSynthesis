84ed356b5d779d1b343158955ef00371a59167eb
bbdaria <dashayakhliel@gmail.com>
2025-06-17 22:12:56 +0300

    lab4

--- lab4/gcd.v
+++ lab4/gcd.v
@@ -18,8 +18,20 @@ Eval simpl in fun x => 1 + x.
 Eval simpl in fun x => x + 1.
 
 Lemma plus_one : forall x, x + 1 = S x.
+Proof.
+  intro x.
+  induction x.
+  - reflexivity.
+  - simpl. rewrite IHx. reflexivity.
+Qed.
 
 Lemma plus_one' : forall x y, x + S y = S x + y.
+Proof.
+  intros x y.
+  induction x.
+  - simpl. reflexivity.
+  - simpl. rewrite IHx. reflexivity.
+Qed.
 
 (* Definition of divisibility + some syntactic sugar *)
 Definition divides a b := exists k, a * k = b.
@@ -30,6 +42,12 @@ Check Nat.add_comm.
 Check Nat.mul_comm.
   
 Lemma divides_refl : forall n, (n | n).
+Proof.
+  intros n.
+  exists 1.
+  simpl. rewrite Nat.mul_1_r.
+  reflexivity.
+Qed.
 
 
 
@@ -44,8 +62,26 @@ Section Gcd.
 
   (* First some basic properties of gcd *)
   Theorem is_gcd_refl : forall z, is_gcd z z z.
+  Proof.
+    intros z.
+    split.
+    - exists 1. apply Nat.mul_1_r.
+    - split.
+      + exists 1. apply Nat.mul_1_r.
+      + intros z' [k1 H1] [k2 H2].
+        (* Goal: ∃k, z' * k = z *)
+        (* From H1: z' * k1 = z *)
+        exists k1. exact H1.
+  Qed.
+
 
   Theorem is_gcd_comm : forall a b z, is_gcd a b z -> is_gcd b a z.
+  Proof.
+    intros a b z [H1 [H2 H3]].
+    split; [assumption | split; [assumption |]].
+    intros z' D1 D2.
+    apply H3; assumption.
+  Qed.
 
   
   (* -- this is a simplified version of Euclid -- *)
@@ -63,8 +99,46 @@ Section Gcd.
   Search (_ * (_ - _)).       (* if I hadn't told you *)
 
   Lemma gcd_step_aux : forall a b z, is_gcd a b z -> is_gcd (a + b) b z.
- 
+  Proof.
+      intros.
+      unfold is_gcd.
+      destruct H.
+      destruct H0.
+      repeat split.
+      - destruct H.
+        destruct H0.
+        exists (x + x0).
+        rewrite Nat.mul_add_distr_l.
+        rewrite H.
+        rewrite H0.
+        reflexivity.
+      - assumption.
+      - intros.
+        apply H1.
+        destruct H2.
+        destruct H3.
+        exists (x - x0).
+        rewrite Nat.mul_sub_distr_l.
+        rewrite H2.
+        rewrite H3.
+        rewrite add_sub.
+        reflexivity.
+        assumption.
+  Qed.
+  
+  
+ (*couldnt make this work, sorry uwu*)
   Theorem gcd_pcorrect : forall a b z, gcd a b z -> is_gcd a b z.
+  Proof.
+    intros a b z H.
+    induction H.
+    - apply is_gcd_refl.
+    - apply gcd_step_aux. assumption.
+    - apply is_gcd_comm.               
+      apply gcd_step_aux.              
+      apply is_gcd_comm.                
+      assumption.                        
+  Qed.
 
 
 End Gcd.
